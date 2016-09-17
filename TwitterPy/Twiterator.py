5cf93bc54ccc1b609e512e5a8671736cd9f968c8
Thomas Laetsch <tom.laetsch@gmail.com>
2016-09-17 16:46:38 -0400

    Added tweet_lat_lon function to TwitterTools.py and starting building class Geo_Twiterator in Twiterator.py to return longitude and latitude iteratively

--- TwitterPy/Twiterator.py
+++ TwitterPy/Twiterator.py
@@ -3,6 +3,7 @@ from os import listdir
 from os.path import isfile, isdir, join, basename, dirname
 from gensim.models.doc2vec import TaggedDocument
 from .TweetTokenizer2 import TweetTokenizer
+from .TwitterTools import tweet_lat_lon
 import json
 
 def files_from_list(files_path_dir):
@@ -48,8 +49,9 @@ class Token_Twiterator(object):
         def __init__(self,
                      jsonfiles,
                      text_processor = 'TweetTokenizer',
+                     token_condition = lambda x: True,
                      stop_words = set(),
-                     condition = lambda x: True,
+                     alt_ret = [], #alternate keys to return
                      verbose = False,
                      **text_processor_kwargs):
             self._JSON_Line_Iterator = JSON_Line_Iterator(jsonfiles, verbose)
@@ -57,15 +59,25 @@ class Token_Twiterator(object):
                 self.text_processor = TweetTokenizer(**text_processor_kwargs).tokenize
             else:
                 self.text_processor = text_processor
+            self.token_condition = token_condition
             self.stop_words = stop_words
-            self.condition = condition
+            self.alt_ret = alt_ret
 
         def __iter__(self):
             for tweet in self._JSON_Line_Iterator:
                 try:
                     proc_text = [word for word in self.text_processor(tweet['text']) if word not in self.stop_words]
-                    if self.condition(proc_text):
-                        yield proc_text
+                    if self.token_condition(proc_text):
+                        if len(self.alt_ret) > 0:
+                            ret_dict = dict()
+                            for key in self.alt_ret:
+                                try:
+                                    ret_dict[key] = tweet[key]
+                                except KeyError:
+                                    pass
+                            yield proc_text, ret_dict
+                        else:
+                            yield proc_text
                     else:
                         pass
                 except KeyError:
@@ -75,20 +87,48 @@ class D2V_Twiterator(object):
     def __init__(self,
                  jsonfiles,
                  text_processor = 'TweetTokenizer',
+                 token_condition = lambda x: True,
                  stop_words = set(),
-                 condition = lambda x: True,
                  d2v_label = "DOC_VEC",
                  verbose = False,
                  **text_processor_kwargs):
-            self._twiterator = Tweet_Text_Twiterator(jsonfiles,
-                                                     text_processor,
-                                                     stop_words,
-                                                     condition,
-                                                     verbose,
-                                                     **text_processor_kwargs)
+            self._twiterator = Token_Twiterator(jsonfiles,
+                                                text_processor,
+                                                token_condition,
+                                                stop_words,
+                                                verbose,
+                                                **text_processor_kwargs)
             self.d2v_label = d2v_label
 
     def __iter__(self):
         for i, text_tokens in enumerate(self._twiterator):
             doc_label = "%s_" % i + self.d2v_label
             yield TaggedDocument(words = text_tokens, tags = [doc_label])
+
+class Geo_Twiterator(object):
+    def __init__(self,
+                 jsonfiles,
+                 ret_wo_geo = False, #bool, return even if not geotagged?
+                 alt_ret = [], #list of tweet keys to return other than geo
+                 verbose = False):
+        self._JSON_Line_Iterator = JSON_Line_Iterator(jsonfiles, verbose)
+        self.ret_wo_geo = ret_wo_geo
+        self.alt_ret = alt_ret
+        self.verbose = verbose
+
+    def __iter__(self):
+        for tweet in self._JSON_Line_Iterator:
+            lat, lon = tweet_lat_lon(tweet, self.verbose)
+            if lat == None or lon == None:
+                if not self.ret_wo_geo:
+                    continue #skip to the next iteration
+            if len(self.alt_ret) > 0:
+                ret_dict = dict()
+                for key in self.alt_ret:
+                    try:
+                        ret_dict[key] = tweet[key]
+                    except KeyError:
+                        ret_dict[key] = None
+                yield lat, lon, ret_dict
+            else:
+                yield lat, lon
--- TwitterPy/TwitterTools.py
+++ TwitterPy/TwitterTools.py
@@ -1,22 +1,38 @@
 from __future__ import print_function
-import json
 
-def in_bounding_box(tweet, box = [-74.265,40.49,-73.655,40.93]):
+def tweet_lat_lon(tweet, verbose = False):
+    try:
+        return tweet['geo']['coordinates']
+    except Exception as e:
+        if verbose:
+            print("Exception %s retrieving geotag.")
+        return None, None
+
+def in_bounding_box(tweet,
+                    box = [-74.265,40.49,-73.655,40.93], #default NYC
+                    verbose = False):
     lonmin = box[0]
     lonmax = box[2]
     latmin = box[1]
     latmax = box[3]
-    try:
-        if tweet['geo'] != None:
-            try:
-                lon = tweet['geo']['coordinates'][1]
-                lat = tweet['geo']['coordinates'][0]
-                if lonmin <= lon <= lonmax and latmin <= lat <= latmax:
-                    return True
-            except KeyError:
-                #'coordinates' not in tweet['geo'].keys()
-                return False
-    except KeyError:
-        #'geo' not in tweet.keys()
-        return False
+
+    lat, lon = tweet_lat_lon(tweet, verbose)
+    if lat != None and lon != None:
+        if lonmin <= lon <= lonmax and latmin <= lat <= latmax:
+            return True
     return False
+
+    #try:
+    #    if tweet['geo'] != None:
+    #        try:
+    #            lon = tweet['geo']['coordinates'][1]
+    #            lat = tweet['geo']['coordinates'][0]
+    #            if lonmin <= lon <= lonmax and latmin <= lat <= latmax:
+    #                return True
+    #        except KeyError:
+                #'coordinates' not in tweet['geo'].keys()
+    #            return False
+    #except KeyError:
+        #'geo' not in tweet.keys()
+    #    return False
+    #return False
