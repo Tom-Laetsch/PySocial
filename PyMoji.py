48b5faf2b56023d2d21a65a070c69dff5f5f0a65
Thomas Laetsch <tom.laetsch@gmail.com>
2016-10-10 12:57:00 -0400

    Updated PyMoji to be more 'pythonic' via attribute settings and property tags

--- PyMoji.py
+++ PyMoji.py
@@ -16,16 +16,11 @@ class PyMoji(object):
     def __init__( self,
                   try_load_first = True,
                   save_on_update = True,
-                  version = -1 ):
-        self.version = -1
-        self.emoji_list = None
-        if try_load_first:
-            self.emoji_list = self.load_emoji_list( version = -1, verbose = False )
-        if self.emoji_list == None:
-            self.emoji_list = self.update_emoji_list( save_pickle = save_on_update )
-        self.emoji_regex = self.get_emoji_regex()
+                  version = -1,
+                  verbose = True ):
+        self._attr_setter( try_load_first, save_on_update, version, verbose )
 
-    def update_emoji_list( self, save_pickle = True ):
+    def _update_emoji_list( self, save_on_update = True ):
         emoji_url = 'http://unicode.org/emoji/charts/full-emoji-list.html'
         idx_class = 'rchars'
         emoji_class = 'chars'
@@ -46,8 +41,9 @@ class PyMoji(object):
             except AttributeError:
                 pass
         emoji_list = sorted(emoji_list, key = lambda x: len(x[0]), reverse = True)
-        if save_pickle:
-            filename = '%s_emoji_list.pickle' % datetime.isoformat(datetime.now())
+        version_date = datetime.now().date().isoformat()
+        if save_on_update:
+            filename = '%s_emoji_list.pickle' % version_date
             filepath = join(save_dir, filename)
             try:
                 with open(filepath, 'wb') as fout:
@@ -64,9 +60,9 @@ class PyMoji(object):
                             fsaved.write(filename)
             except IOError:
                 print("IOError: Could not save emoji list pickle file.")
-        return emoji_list
+        return emoji_list, version_date
 
-    def load_emoji_list( self, version = -1, verbose = False ):
+    def _load_emoji_list( self, version = -1, verbose = False ):
         # version is a negative int with -1 most recent, and -n the nth previously saved version.
         filenames = []
         fsaved_names = join(save_dir, emoji_list_version_fp)
@@ -75,8 +71,8 @@ class PyMoji(object):
                 for name in fsaved:
                     filenames.append(name)
         except IOError:
-            if verbose: print("No saved emoji list files found. To create one, run update_emoji_list().")
-            return None
+            if verbose: print("No saved emoji list files found.")
+            return None, None
         if abs(version) > len(filenames): version = 0
         filename = filenames[version]
         try:
@@ -86,17 +82,52 @@ class PyMoji(object):
                     emoji_list = pickle.load(fin, encoding = 'latin1')
                 except TypeError:
                     emoji_list = pickle.load(fin)
-            if verbose: print("Imported emoji list from date: %s" % filename[:-18])
+            version_date = filename[:10]
+            if verbose: print("Imported emoji list from date: %s" % version_date)
         except IOError:
-            if verbose: print("No saved emoji list files found. To create one, run update_emoji_list().")
-            return None
-        return emoji_list
+            if verbose: print("No saved emoji list files found. To create one, run _update_emoji_list().")
+            return None, None
+        return emoji_list, version_date
 
-    def get_emoji_regex( self ):
+
+    def _emoji_regex_maker( self ):
         emoji_re = ''
-        for e in self.emoji_list:
+        for e in self._emoji_list:
             emoji_re += '(?:' + e[0] + ')|'
         #asterisks cause trouble
         emoji_re = re.sub(r'\*', r'\*', emoji_re)
         #remove trailing or |
         return emoji_re[:-1]
+
+    def _attr_setter( self,
+                     try_load_first = True,
+                     save_on_update = True,
+                     version = -1,
+                     verbose = False ):
+        emoji_list = None
+        if try_load_first:
+            emoji_list, version_date = self._load_emoji_list( version = version, verbose = verbose )
+        if emoji_list == None:
+            if verbose: print("Attempting to update emoji_list...")
+            emoji_list, version_date = self._update_emoji_list( save_on_update = save_on_update )
+            if verbose: print("emoji_list now updated.")
+        setattr(self, '_emoji_list', emoji_list)
+        setattr(self, '_version_date', version_date)
+
+        emoji_regex = self._emoji_regex_maker()
+        setattr(self, '_emoji_regex', emoji_regex)
+
+    def update_emoji_list( self, save_on_update = True ):
+        self._attr_setter( try_load_first = False, save_on_update = save_on_update )
+
+    @property
+    def emoji_list( self ):
+        return self._emoji_list
+
+    @property
+    def emoji_regex( self ):
+        return self._emoji_regex
+
+    @property
+    def version_date( self ):
+        return self._version_date
--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
 import sys, os
 sys.path.append(os.path.dirname(__file__))
 from .PyMoji import PyMoji
-from .TwitterPy import Twiterator, TwitterTools, TweetTokenizer2
+#import TwitterPy
+#from .TwitterPy import Twiterator, TwitterTools, TweetTokenizer2
