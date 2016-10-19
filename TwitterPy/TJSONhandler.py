c1339f052aaea08b21c6d4049a8095171bbbe240
Thomas Laetsch <tom.laetsch@gmail.com>
2016-10-19 15:11:04 -0400

    Updated TJSONhandler.py

--- TwitterPy/TJSONhandler.py
+++ TwitterPy/TJSONhandler.py
@@ -1,7 +1,10 @@
 from __future__ import absolute_import, print_function, division
 from os import listdir
 from os.path import isfile, isdir, join, basename, dirname
-import json
+import json, re, pprint
+from PySocial import TweetTokenizer, EMOJI_RE, EMOJI_LIST
+
+pp = pprint.PrettyPrinter(indent = 4)
 
 #helper function: makes a list of filesnames from passed argument
 def files_from_list(files_path_dir):
@@ -27,7 +30,7 @@ class JSON_Line_Iterator(object):
     #an iterator for tweets from json file
     def __init__(self, jsonfiles, verbose = False):
         self.jsonfiles = files_from_list(jsonfiles)
-        if self.jsonfiles == None:
+        if self.jsonfiles is None:
             print("No JSON files found.")
         self.verbose = verbose
     def __iter__(self):
@@ -37,10 +40,90 @@ class JSON_Line_Iterator(object):
                     for i,line in enumerate(jf):
                         try:
                             yield json.loads(line)
-                        except ValueError:
+                        except Exception as e:
                             if self.verbose:
-                                print("JSON Decode Error with line %d in file %s." % (i, basename(f)))
+                                print("Exception on line %d of file %s: \n-%s" % (i,basename(f),e))
                             pass
             except IOError:
-                IOError_message(f)
+                if self.verbose: IOError_message(f)
                 yield None
+
+def emojiInText( tweet, verbose = False ):
+    try:
+        return len( re.findall(EMOJI_RE, tweet['text']) ) > 0
+    except Exception as e:
+        if verbose:
+            print("Exception: %s" % e)
+            print("----------------")
+            print("In tweet: %s" % tweet)
+            print("----------------")
+            print("")
+        return False
+
+def placeInCountry( tweet, country_code = 'US', verbose = False):
+    try:
+        place = tweet['place']
+        if isinstance(place,dict):
+            cc = place.get('country_code')
+            return cc == country_code
+        return False
+    except Exception as e:
+        if verbose:
+            print("Exception: %s" % e)
+            print("----------------")
+            print("In tweet: %s" % tweet)
+            print("----------------")
+            print("")
+        return False
+
+class JSONTwiterator( object ):
+    def __init__( self, tjsonfiles, criteria = ['placeInCountry'], verbose = False ):
+        self._json_line_iterator = JSON_Line_Iterator( tjsonfiles, verbose )
+        self.criteria_dict = dict()
+        self.criteria = criteria
+        self.set_criteria( criteria )
+
+
+    def set_criteria( self, criteria ):
+        if criteria is None:
+            return False
+        criteria = list(criteria)
+        available = {
+                        'emojiInText' : {'function': emojiInText,
+                                         'kwargs': {
+                                                     'verbose': False
+                                                   }
+                                        },
+                        'placeInCountry': {'function': placeInCountry,
+                                           'kwargs': {
+                                                      'country_code': 'US',
+                                                      'verbose': False
+                                                     }
+                                          }
+                     }
+        unavailable = [c for c in criteria if c not in available.keys()]
+        if len(unavailable) > 0:
+            print('Not all criteria give are available.')
+            print('Available criteria are:')
+            for key in available.keys():
+                print('- %s' % key)
+                for kwval, val in available[key]['kwargs'].items():
+                    print('\t-kwargs: %s | default = %s' % (kwval,val))
+            print("Please use set_criteria( criteria = list_of_criteria_names ). No criteria set.")
+            return False
+        else:
+            self.criteria_dict.update({key: available[key] for key in criteria})
+            return True
+
+    def _criteria_fun( self, tweet ):
+        for _, criteria in self.criteria_dict.items():
+            if isinstance(criteria, dict):
+                function = criteria.get('function', lambda x: True)
+                kwargs = criteria.get('kwargs', dict())
+                if not function( tweet, **kwargs ): return False
+        return True
+
+    def __iter__( self ):
+        for tweet in self._json_line_iterator:
+            if self._criteria_fun( tweet ):
+                yield tweet
